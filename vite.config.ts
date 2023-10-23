42ab59aaa467678efc9931bea64782d60cd87a5e
Zamiliukhin Valentyn <valentynzamiliukhin@gmail.com>
2023-10-23 21:58:41 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: path.resolve(__dirname, '/src')
+      src: path.resolve(__dirname, './src')
     }
   },
   css: {
