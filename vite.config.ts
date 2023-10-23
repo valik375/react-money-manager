309e5a5f379318956291a9d79276303390503b6f
Zamiliukhin Valentyn <valentynzamiliukhin@gmail.com>
2023-10-23 21:53:21 +0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: '/src'
+      src: path.resolve(__dirname, '/src')
     }
   },
   css: {
